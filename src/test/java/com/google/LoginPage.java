1bec1a3c73cf199ab42eab73ee5498fb34cd657c
rachitsaurabh <rachit.saurabh123@gmail.com>
2020-04-27 14:15:41 +0530

    first git commit for my selenium code

---
+++ src/test/java/com/google/LoginPage.java
@@ -0,0 +1,26 @@
+
+package com.google;
+
+import org.testng.annotations.Test;
+
+public class LoginPage {
+	
+	@Test
+	public void GoogleTitleTest() {
+		System.out.println("This is my google page");
+	}
+	
+	@Test
+	public void LoginTest() {
+		System.out.println("Login Page");
+	}
+	
+	@Test
+	public void homepage() {
+		System.out.println("Home Page");
+	}
+	@Test
+	public void signout() {
+		System.out.println("Sign Out");
+	}
+}
